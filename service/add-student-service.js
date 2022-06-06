7ec5adc6ab500b2881588dad087aa5583d2ef12e
Elias Volonakis <envolonakis@gmail.com>
2022-06-06 16:36:40 -0400

    Added Detailed Formatting to PDF Service

---
+++ service/add-student-service.js
@@ -0,0 +1,2 @@
+// Add Student to MongoDB Collection
+// Student: firstName: str, lastName: str, studentEmail: str, parentEmail: [] active: bool 
--- service/pdf-service.js
+++ service/pdf-service.js
@@ -1,15 +1,37 @@
+require('dotenv').config();
 const PDFDocument = require('pdfkit');
+let date = new Date();
+const dateString = 'Date: ' + date.toLocaleString('default', { month: 'long' }) + ', ' + date.getDate() + ', ' + date.getFullYear();
 
 function buildPDF(dataCallback, endCallback) {
     createInvoice(dataCallback, endCallback)
 
 }
 
-function createInvoice(dataCallback, endCallback) {
+function createInvoice(dataCallback, endCallback, studentName = "BOB") {
     const doc = new PDFDocument();
     doc.on('data', dataCallback);
     doc.on('end', endCallback);
-    doc.fontSize(25).text('Some Heading!', 100, 100);
+    doc.font('Times-Bold').fontSize(12).text(process.env.OWNER_NAME, 50, 50);
+    doc.font('Times-Bold').fontSize(12).text(process.env.OWNER_HOME_ADDRESS, 50, 62);
+    doc.font('Times-Bold').fontSize(12).text(process.env.OWNER_HOME_CITY_PROVINCE_PC, 50, 74);
+    doc.font('Times-Bold').fontSize(12).text('T: ' + process.env.OWNER_TELEPHONE, 50, 86);
+    doc.font('Times-Bold').fontSize(12).text('E: ' + process.env.OWNER_EMAIL, 50, 98);
+    doc.fillOpacity(0.5)
+    doc.font('Times-Roman').fontSize(27.5).text('INTERIM', 50, 250);
+    doc.font('Times-Roman').fontSize(27.5).text('INVOICE', 50, 278);
+    doc.fillOpacity(1)
+    doc.font('Times-Roman').fontSize(12).text(dateString, 420, 290);
+    doc.font('Times-Bold').fontSize(14).text("Student: " + studentName, 50, 315);
+    
+    doc.polygon([50, 340], [50, 400], [(doc.page.width - 50) / 2, 400], [(doc.page.width - 50) / 2, 340])
+    .stroke();
+    doc.polygon([(doc.page.width - 50) / 2, 340], [(doc.page.width - 50) / 2, 400], [doc.page.width - 50, 400], [doc.page.width - 50, 340])
+    .stroke();
+    doc.polygon([50, 360], [50, 400], [doc.page.width - 50, 400], [doc.page.width - 50, 360])
+    .stroke();
+    doc.font('Times-Roman').fontSize(12).text(dateString, 420, 290);
+
     doc.end();
 }
 
