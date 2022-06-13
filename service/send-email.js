3f560cdd2683d5ca9e309f91792fc06796f832b1
Elias Volonakis <envolonakis@gmail.com>
2022-06-13 15:55:02 -0400

    send-email working as required

--- service/send-email.js
+++ service/send-email.js
@@ -7,7 +7,6 @@ console.log("CLIENT_SECRET: " + process.env.CLIENT_SECRET);
 console.log("CLIENT_REDIRECT_URI: " + process.env.REDIRECT_URI);
 console.log("REFRESH_TOKEN: " + process.env.REFRESH_TOKEN);
 
-// const oAuth2 = google.auth.OAuth2;
 const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
 console.log("oAuth2Client: " + oAuth2Client);
 
@@ -15,34 +14,37 @@ oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
 
 async function sendMail() {
     try {
-        const accessToken = await oAuth2Client.getAccessToken()
-        console.log("Access Toke: " + accessToken);
+        const accessToken = await oAuth2Client.getAccessToken();
+        console.log(accessToken.token);
         const transport = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                type: 'OAuth2',
-                user: process.env.OWNER_EMAIL,
-                clientId: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
-                accessToken: accessToken
-            }
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+            type: 'OAuth2'
+          }
         });
-
+    
         const mailOptions = {
-            from: process.env.OWNER_EMAIL,
-            to: process.env.RECIPIENT,
-            subject: "Test Email API Subject",
-            text: "Test Email API Text",
-            html: "<h1> Test Email API HTML </h1>"
+          from: 'envolonakis@gmail.com',
+          to: 'envolonakis@gmail.com',
+          subject: "Test Email API Subject",
+          text: "Test Email API Text",
+          html: "<h1> Test Email API HTML </h1>",
+          auth: {
+            user: process.env.OWNER_EMAIL,
+            accessToken: accessToken.token
+          }
         }
-
+    
         const result = await transport.sendMail(mailOptions);
         return result;
-    } catch(error) {
+        console.log("Email sent successfully with messageID:" + result.messageId + " and response " + result.response); 
+      } catch (error) {
+        console.log(error.stack);  
         return error;
-    }
+      }
 }
 
     
-sendMail().then(result => console.log("Email is sent: ", result))
-.catch((error) => console.log(error.message));
+sendMail()
