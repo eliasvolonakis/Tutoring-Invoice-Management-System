d6f6e1bab0360edc5887746218609772024e23fd
Elias Volonakis <envolonakis@gmail.com>
2022-06-18 10:02:12 -0400

    Updated Subject & Body to send-email

--- service/send-email.js
+++ service/send-email.js
@@ -2,6 +2,10 @@ const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 require('dotenv').config();
 
+let date = new Date();
+const current_date = date.toLocaleString('default', { month: 'long' });
+const subject = current_date + " Invoice";
+
 console.log("CLIENT_ID: " + process.env.CLIENT_ID);
 console.log("CLIENT_SECRET: " + process.env.CLIENT_SECRET);
 console.log("CLIENT_REDIRECT_URI: " + process.env.REDIRECT_URI);
@@ -24,13 +28,13 @@ async function sendMail() {
             type: 'OAuth2'
           }
         });
-    
+        
         const mailOptions = {
           from: process.env.OWNER_EMAIL,
           to: 'envolonakis@gmail.com',
-          subject: "Test Email API Subject",
-          text: "Test Email API Text",
-          html: "<h1> This is a test email </h1>",
+          subject: subject,
+          text: "Good morning, \n \n Attached to this email is STUDENTS tutoring invoice for all " + current_date + " tutoring sessions. Whenever it's convenient for you, please complete the payment via e-transfer to this email address",
+          //html: "<p>Good morning,</p> <p>Attached to this email is STUDENTS tutoring invoice for all " + current_date + " tutoring sessions. Whenever it's convenient for you, please complete the payment via e-transfer to this email address. </p>",
           auth: {
             user: process.env.OWNER_EMAIL,
             accessToken: accessToken.token
