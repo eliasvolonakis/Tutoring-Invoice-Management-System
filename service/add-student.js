41145515cf28545498d8b172a2e4a65d60a7bb3c
Elias Volonakis <envolonakis@gmail.com>
2022-06-18 09:43:44 -0400

    Reorganized Data & Implemented add-student service

--- service/add-student.js
+++ service/add-student.js
@@ -1,4 +1,24 @@
-// Add Student to MongoDB Collection
-// Student: firstName: str, lastName: str, studentEmail: str, parentEmail: [] active: bool 
-
 require('dotenv').config();
+const students_json = require('../data/students.json'); 
+const MongoClient = require('mongodb').MongoClient;
+const mongo_url = process.env.MONGO_URL;
+
+MongoClient.connect(mongo_url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+
+  students_json.forEach(student => {
+    // Replace a students existing document if anything has changed based on the internal students.json
+    // By setting upsert to true, if the document does not already exist, it will be added to the collection
+    try {
+        db.restaurant.replaceOne(
+           { "studentName" : student[studentName] },
+           student,
+           { upsert: true }
+        );
+    } catch (e){
+        console.log(e);
+    }
+  });
+  db.close();
+});
