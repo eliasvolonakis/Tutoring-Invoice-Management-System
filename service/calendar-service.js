d7d25e42e8237dd2033d61f6b135434252b0a14b
Elias Volonakis <envolonakis@gmail.com>
2022-06-22 15:35:50 -0400

    Updated Format of Date Text

--- service/calendar-service.js
+++ service/calendar-service.js
@@ -22,6 +22,12 @@ const calendar = google.calendar({version : "v3", auth});
 // Replace later with specific times 
 const timeMin = new Date(2022, 6, 01);
 const timeMax = new Date(2022, 8, 30);
+
+// Replace above with this for first and last day of month
+// var date = new Date();
+// var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+// var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
 let sessions = "";
 
 function listEvents() {
@@ -37,16 +43,16 @@ function listEvents() {
     const events = res.data.items;
     if (events.length) {
       console.log(`Events between ${timeMin.toISOString()} - ${timeMax.toISOString()}`);
-      // for (let event of events) {
-      //   const start = event.start.dateTime || event.start.date;
-      //   const end = event.end.dateTime || event.end.date;
-      //   outputMonthlySessions(`${event.summary}: ${start} - ${end} \n`);
-      // }
       events.map(event => {
-        const start = event.start.dateTime || event.start.date;
-        const end = event.end.dateTime || event.end.date;
-        sessions += `${event.summary}: ${start} - ${end} \n`;
-        //outputMonthlySessions(`${event.summary}: ${start} - ${end} \n`);
+        const start = new Date(event.start.dateTime) || new Date(event.start.date);
+        const end = new Date (event.end.dateTime) || new Date(event.end.date);
+        try{
+          sessions += `${event.summary}: ${start.toLocaleString('default', { month: 'long' })} ${start.getDate()}, ${start.getFullYear()}: ${start.getHours()}:${String(start.getMinutes()).padStart(2, '0')} - ${end.getHours()}:${String(end.getMinutes()).padStart(2, '0')} \n`;
+          console.log(sessions);
+        }
+        catch(err) {
+          console.log(err)
+        }
       });
       outputMonthlySessions(sessions);
     } else {
