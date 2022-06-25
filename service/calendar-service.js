988953e699df69e46db58b335cc6e43efd55887b
Elias Volonakis <envolonakis@gmail.com>
2022-06-24 23:54:47 -0400

    Added getSessionString

--- service/calendar-service.js
+++ service/calendar-service.js
@@ -1,7 +1,6 @@
 const credentials = require('./../credentials');
+const utils = require('./../utils/utils.js');
 const { google } = require('googleapis');
-const fs = require('fs');
-const SESSIONS_PATH = credentials["SESSIONS_PATH"];
 const CREDENTIALS = credentials["CALENDAR_CREDENTIALS"];
 const CALENDAR_ID = credentials["CALENDAR_ID"];
 
@@ -37,44 +36,33 @@ function listEvents() {
     singleEvents: true,
     orderBy: 'startTime',
   }, (err, res) => {
-    console.log("Beginning list Events")
     if (err) return console.log('The API returned an error: ' + err);
     const events = res.data.items;
     if (events.length) {
-      console.log(`Events between ${timeMin.toISOString()} - ${timeMax.toISOString()}`);
+      console.log(`Events between ${timeMin.getMonth()}/${timeMin.getDate()}/${timeMin.getFullYear()} - ${timeMax.getMonth()}/${timeMax.getDate()}/${timeMax.getFullYear()}`);
       events.map(event => {
         const start = new Date(event.start.dateTime) || new Date(event.start.date);
         const end = new Date (event.end.dateTime) || new Date(event.end.date);
         try{
             // Assume that all Tutoring sessions contains the string "Tutoring" in event name
-            if (event.summary.includes("Tutoring")){
-                sessions += `${event.summary}!${start.toLocaleString('default', { month: 'long' })} ${start.getDate()}, ${start.getFullYear()}: ${start.getHours()}:${String(start.getMinutes()).padStart(2, '0')} - ${end.getHours()}:${String(end.getMinutes()).padStart(2, '0')}!${getSessionDifference(start, end)}  \n`;
+            if (event.summary.includes("Tutoring")) {
+              console.log(utils);
+              sessions += getSessionString(event, start, end);
             }
         }
         catch(err) {
           console.log(err)
         }
       });
-      outputMonthlySessions(sessions);
+      utils.outputMonthlySessions(sessions);
     } else {
       console.log('No upcoming events found.');
     }
   });
 }
 
-function outputMonthlySessions(content) {
-  fs.writeFile(SESSIONS_PATH, content, err => {
-    if (err) {
-      console.error(err)
-    }
-  });
-}
-
-function getSessionDifference(start, end) {
-  let hoursDifference = end.getHours() - start.getHours();
-  let minutesDifference = (end.getMinutes() - start.getMinutes()) / 60;
-  let totalDifference = hoursDifference + minutesDifference;
-  return String(totalDifference);
+function getSessionString(event, start, end) {
+  return `${event.summary}!${start.toLocaleString('default', { month: 'long' })} ${start.getDate()}, ${start.getFullYear()}: ${start.getHours()}:${String(start.getMinutes()).padStart(2, '0')} - ${end.getHours()}:${String(end.getMinutes()).padStart(2, '0')}!${utils.getSessionDifference(start, end)}\n`;
 }
 
 listEvents()
